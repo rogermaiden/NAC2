de98641f60418c40bb333ca9b270b5f55b0e1c9b
rogerio vilela da silva filho <30608971+rogermaiden@users.noreply.github.com>
2019-11-21 20:48:57 -0300

    Add files via upload

---
+++ NAC2/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+    >
+      <v-btn text to="/">
+        <span>Chuck Norris Facts</span>    
+      </v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon @click="reloadPage">
+        <v-icon>refresh</v-icon>
+      </v-btn>
+
+    </v-app-bar>
+
+    <v-content>
+      <router-view />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  components: {
+  },
+
+  methods: {
+    reloadPage(){
+      window.location.reload()
+    }
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ NAC2/src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import vuetify from './plugins/vuetify';
+import store from './config/store'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import './store/chuckfacts.store'
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  vuetify,
+  render: h => h(App)
+}).$mount('#app')
